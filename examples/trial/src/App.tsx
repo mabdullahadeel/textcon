58608e016f6903841919dce978ab994f116378ad
Abdullah Adeel <easyautosol@gmail.com>
2023-06-01 02:38:18 +0500

    🤝 updated state comparison to simple strict equal  - by default states will be compared by strict equal comparison  - export function `shallow` for shallow comparison - single level  - trial app updates

--- examples/trial/src/App.tsx
+++ examples/trial/src/App.tsx
@@ -1,7 +1,8 @@
 import reactLogo from "./assets/react.svg";
 import viteLogo from "/vite.svg";
 import "./App.css";
-import { ActionablePayload, createContextStore } from "../../../src/create";
+// import { ActionablePayload, createContextStore } from "../../../src/create";
+import { ActionablePayload, createContextStore } from "textcon";
 import { produce } from "immer";
 
 const { Provider, useStore, useActions } = createContextStore(
@@ -13,7 +14,6 @@ const { Provider, useStore, useActions } = createContextStore(
       email: "",
     },
     count: 0,
-    hobbies: ["football", "basketball"],
   },
   {
     increment: ({ set }) => {
@@ -24,21 +24,89 @@ const { Provider, useStore, useActions } = createContextStore(
       );
     },
     decrement: ({ set }) => {
-      set(produce((state) => ({ count: state.count - 1 })));
+      set(
+        produce((state) => {
+          state.count -= 1;
+        })
+      );
     },
     incrementBy: ({ set }, action: ActionablePayload<{ by: number }>) => {
       console.log(action?.payload || "No payload");
       set(
-        produce((state) => ({
-          count: state.count + (action?.payload?.by || 10),
-        }))
+        produce((state) => {
+          state.count = state.count + (action?.payload?.by || 10);
+        })
+      );
+    },
+    updateFirstName: ({ set }, action: ActionablePayload<string>) => {
+      set(
+        produce((state) => {
+          state.user.firstName = action?.payload || "";
+        })
+      );
+    },
+    updateLastName: ({ set }, action: ActionablePayload<string>) => {
+      set(
+        produce((state) => {
+          state.user.lastName = action?.payload || "";
+        })
       );
     },
   }
 );
 
+function FirstNameDisplay() {
+  const { get: firstName } = useStore((state) => state.user.firstName);
+
+  return <div>{firstName}</div>;
+}
+
+function LastNameDisplay() {
+  const { get: lastName } = useStore((state) => state.user.lastName);
+
+  return <div>{lastName}</div>;
+}
+
+function FirstNameUpdater() {
+  const { updateFirstName } = useActions();
+
+  return (
+    <button onClick={() => updateFirstName("Jane " + Math.random())}>
+      Update first name
+    </button>
+  );
+}
+
+function LastNameUpdater() {
+  const { updateLastName } = useActions();
+
+  return (
+    <button onClick={() => updateLastName("Jackson " + Math.random())}>
+      Update last name
+    </button>
+  );
+}
+
+function UserDisplay() {
+  return (
+    <>
+      <FirstNameDisplay />
+      <LastNameDisplay />
+    </>
+  );
+}
+
+function UserUpdater() {
+  return (
+    <>
+      <FirstNameUpdater />
+      <LastNameUpdater />
+    </>
+  );
+}
+
 function App() {
-  const { get } = useStore();
+  const { get: count } = useStore((state) => state.count);
   const { increment, decrement, incrementBy } = useActions();
 
   return (
@@ -53,7 +121,7 @@ function App() {
       </div>
       <h1>Vite + React</h1>
       <div className="card">
-        <button onClick={() => increment()}>increment: {get.count}</button>
+        <button onClick={() => increment()}>increment: {count}</button>
         <button onClick={() => decrement()}>decrement</button>
         <button onClick={() => incrementBy({ by: 10 })}>By</button>
         <p>
@@ -71,6 +139,8 @@ function Root() {
   return (
     <Provider>
       <App />
+      <UserDisplay />
+      <UserUpdater />
     </Provider>
   );
 }
